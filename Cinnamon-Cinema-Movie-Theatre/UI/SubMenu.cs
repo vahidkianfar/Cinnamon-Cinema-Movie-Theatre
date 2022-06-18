54c9849e58304c51695318750f33c831185728ba
vahidkianfar <vahidkianfar@gmail.com>
2022-06-18 20:21:35 +0100

    Added try-catch block for handling exception on Sub Menu.

--- Cinnamon-Cinema-Movie-Theatre/UI/SubMenu.cs
+++ Cinnamon-Cinema-Movie-Theatre/UI/SubMenu.cs
@@ -1,7 +1,7 @@
-﻿using Cinnamon_Cinema_Movie_Theatre.Manager;
+﻿using Cinnamon_Cinema_Movie_Theatre;
+using Cinnamon_Cinema_Movie_Theatre.Manager;
 using Npgsql;
-
-namespace Cinnamon_Cinema_Movie_Theatre.UI;
+using Cinnamon_Cinema_Movie_Theatre.UI;
 
 public class SubMenu
 {
@@ -11,9 +11,10 @@ public class SubMenu
         connectionToDatabase.Open();
         MovieManager.SetConnection(connectionToDatabase);
         MovieManager.GetMovieDetails();
-        
-        var selectInstructionOption = ConsoleHelper.MultipleChoice(true, $"1. Title: {MovieManager.MovieDetails![0].Item2} (Directed by {MovieManager.MovieDetails[0].Item3})",
-             "2. Back", "3. Exit");
+
+        var selectInstructionOption = ConsoleHelper.MultipleChoice(true,
+            $"1. Title: {MovieManager.MovieDetails![0].Item2} (Directed by {MovieManager.MovieDetails[0].Item3})",
+            "2. Back", "3. Exit");
         connectionToDatabase.Close();
 
         switch (selectInstructionOption)
@@ -30,37 +31,51 @@ public class SubMenu
         }
     }
 
+    
     private static void StartBooking()
     {
-        while (true)
+        try
         {
-            var selectInstructionOption = ConsoleHelper.MultipleChoice(true, $"1. Book Gold Seat (choose your seat)", "2. Book Silver Seat (first available seats)", "3. Back", "4. Back to Main Menu" ,"5. Exit");
-            switch (selectInstructionOption)
+            while (true)
             {
-                case 0:
-                    Console.Write("\nEnter Seat Row  (e.g. A): ");
-                    var inputRow = Convert.ToChar(Console.ReadLine()!);
-                    Console.Write("Enter Seat Column (e.g. 1): ");
-                    var inputColumn = Convert.ToInt32(Console.ReadLine()!);
-                    BookingManager.ReserveGoldSeat(inputRow, inputColumn);
-                    continue;
+                var selectInstructionOption = ConsoleHelper.MultipleChoice(true,
+                    $"1. Book Gold Seat (choose your seat)", "2. Book Silver Seat (first available seats)", "3. Back",
+                    "4. Back to Main Menu", "5. Exit");
+                switch (selectInstructionOption)
+                {
+                    case 0:
+                        Console.Write("\nEnter Seat Row  (e.g. 'A'): ");
+                        var inputRow = Convert.ToChar(Console.ReadLine()!);
+                        Console.Write("Enter Seat Column (e.g. 1): ");
+                        var inputColumn = Convert.ToInt32(Console.ReadLine()!);
+                        BookingManager.ReserveGoldSeat(char.ToUpper(inputRow), inputColumn);
+                        continue;
 
-                case 1:
-                    Console.Write("Enter number of tickets (between 1 to 3) :");
-                    var inputTickets = Convert.ToInt32(Console.ReadLine()!);
-                    BookingManager.ReserveSilverSeat(inputTickets);
-                    continue;
-                case 2:
-                    ShowMoviesMenu();
-                    break;
-                case 3:
-                    MainMenu.Start();
-                    break;
-                case 4:
-                    Environment.Exit(0);
-                    break;
+                    case 1:
+                        Console.Write("Enter number of tickets (between 1 to 3) :");
+                        var inputTickets = Convert.ToInt32(Console.ReadLine()!);
+                        BookingManager.ReserveSilverSeat(inputTickets);
+                        continue;
+                    case 2:
+                        ShowMoviesMenu();
+                        break;
+                    case 3:
+                        MainMenu.Start();
+                        break;
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                }
             }
-            break;
         }
+
+        catch (Exception e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"System Message: {e.Message}");
+            Console.ResetColor();
+            StartBooking();
+        }
+
     }
 }
